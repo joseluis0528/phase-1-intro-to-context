45534438c2982b28f07381b1f0637c3c5ff03bfe
Jose <joseluis0528@gmail.com>
2023-04-20 22:04:11 -0500

    first 4 functions working

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ function createEmployeeRecord(array) {
         title: array[2],
         payPerHour: array[3],
         timeInEvents: [],
-        timeOutEvents: [],
+        timeOutEvents: []
     }
+}
+
+function createEmployeeRecords(array) {
+    return array.map(eployee => createEmployeeRecord(eployee));
+}
+
+function createTimeInEvent(obj, dateStamp) {
+    const timeAndDate = dateStamp.split(' ');
+    obj.timeInEvents = [
+        {
+            type: 'TimeIn',
+            hour: parseInt(timeAndDate[1]),
+            date: timeAndDate[0]
+        }
+    ]
+    return obj;
+}
+
+function createTimeOutEvent(obj, dateStamp) {
+    const timeAndDate = dateStamp.split(' ');
+    obj.timeOutEvents = [
+        {
+            type: 'TimeOut',
+            hour: parseInt(timeAndDate[1]),
+            date: timeAndDate[0]
+        }
+    ]
+    return obj;
 }
